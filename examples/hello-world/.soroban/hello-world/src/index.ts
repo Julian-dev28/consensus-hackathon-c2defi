c8ebadee70883de29114a95f67a61c75bd6a5eb1
Julian Martinez <julian_martinez28@outlook.com>
2024-05-06 22:26:45 -0700

    update example app

---
+++ examples/hello-world/.soroban/hello-world/src/index.ts
@@ -0,0 +1,75 @@
+import { ContractSpec, Address } from '@stellar/stellar-sdk';
+import { Buffer } from "buffer";
+import {
+  AssembledTransaction,
+  ContractClient,
+  ContractClientOptions,
+} from '@stellar/stellar-sdk/lib/contract_client/index.js';
+import type {
+  u32,
+  i32,
+  u64,
+  i64,
+  u128,
+  i128,
+  u256,
+  i256,
+  Option,
+  Typepoint,
+  Duration,
+} from '@stellar/stellar-sdk/lib/contract_client';
+import { Result } from '@stellar/stellar-sdk/lib/rust_types/index.js';
+export * from '@stellar/stellar-sdk'
+export * from '@stellar/stellar-sdk/lib/contract_client/index.js'
+export * from '@stellar/stellar-sdk/lib/rust_types/index.js'
+
+if (typeof window !== 'undefined') {
+    //@ts-ignore Buffer exists
+    window.Buffer = window.Buffer || Buffer;
+}
+
+
+export const networks = {
+  testnet: {
+    networkPassphrase: "Test SDF Network ; September 2015",
+    contractId: "CAOLYG2EYPACZC6IEOT3LCH3HRZCKPHWRD3EAIKABIXCQ4HKDWPAOLH6",
+  }
+} as const
+
+export const Errors = {
+  
+}
+
+export interface Client {
+  /**
+   * Construct and simulate a hello transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  hello: ({to}: {to: string}, options?: {
+    /**
+     * The fee to pay for the transaction. Default: BASE_FEE
+     */
+    fee?: number;
+
+    /**
+     * The maximum amount of time to wait for the transaction to complete. Default: DEFAULT_TIMEOUT
+     */
+    timeoutInSeconds?: number;
+
+    /**
+     * Whether to automatically simulate the transaction when constructing the AssembledTransaction. Default: true
+     */
+    simulate?: boolean;
+  }) => Promise<AssembledTransaction<Array<string>>>
+
+}
+export class Client extends ContractClient {
+  constructor(public readonly options: ContractClientOptions) {
+    super(
+      new ContractSpec([ "AAAAAAAAAAAAAAAFaGVsbG8AAAAAAAABAAAAAAAAAAJ0bwAAAAAAEQAAAAEAAAPqAAAAEQ==" ]),
+      options
+    )
+  }
+  public readonly fromJSON = {
+    hello: this.txFromJSON<Array<string>>
+  }
+}

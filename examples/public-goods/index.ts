36a5ee2764a725a30541caccc64e472bdd8d95b1
Julian Martinez <julian_martinez28@outlook.com>
2024-05-09 10:07:51 -0700

    add bun to public goods example

--- examples/public-goods/index.ts
+++ examples/public-goods/index.ts
@@ -16,24 +16,6 @@ await horizon.friendbot(pubkey).call();
 console.log("created account");
 
 await $`soroban keys add owner --secret-key`.env({
-  ...process.env,import { Horizon, Keypair, Networks } from "@stellar/stellar-sdk";
-import { $ } from "bun";
-
-await $`bun rimraf target/wasm32-unknown-unknown/release .env.local`;
-console.log("cleaned target");
-
-const horizonUrl = "https://horizon-testnet.stellar.org";
-const horizon = new Horizon.Server(horizonUrl, { allowHttp: true });
-
-const keypair = Keypair.random();
-const secret = keypair.secret();
-const pubkey = keypair.publicKey();
-
-await horizon.friendbot(pubkey).call();
-
-console.log("created account");
-
-await $`soroban keys add owner --secret-key`.env({
   ...process.env,
   SOROBAN_SECRET_KEY: secret,
 });
@@ -42,7 +24,7 @@ await $`soroban contract build`;
 console.log("built contracts");
 
 const contractId_1 = (
-  await $`soroban contract deploy --wasm target/wasm32-unknown-unknown/release/soroban_hello_world_contract.wasm --network testnet --source owner`.text()
+  await $`soroban contract deploy --wasm target/wasm32-unknown-unknown/release/soroban_increment_contract.wasm --network testnet --source owner`.text()
 ).replace(/\W/g, "");
 
 console.log("deployed contracts");
@@ -58,28 +40,6 @@ await Bun.write(".env.local", file);
 console.log("✅");
 
 const bindings =
-  await $`soroban contract bindings typescript --wasm target/wasm32-unknown-unknown/release/soroban_hello_world_contract.wasm --id ${contractId_1} --network testnet --output-dir ./.soroban/hello-world --overwrite`.text();
+  await $`soroban contract bindings typescript --wasm target/wasm32-unknown-unknown/release/soroban_increment_contract.wasm --id ${contractId_1} --network testnet --output-dir ./.soroban/incrementor-contract --overwrite`.text();
 bindings;
 console.log("generated bindings");
-
-  SOROBAN_SECRET_KEY: secret,
-});
-
-await $`soroban contract build`;
-console.log("built contracts");
-
-const contractId_1 = (
-  await $`soroban contract deploy --wasm target/wasm32-unknown-unknown/release/soroban_hello_world_contract.wasm --network testnet --source owner`.text()
-).replace(/\W/g, "");
-
-console.log("deployed contracts");
-
-if (!contractId_1) throw new Error("Contracts not deployed");
-
-let file = ``;
-file += `CONTRACT_ID_1=${contractId_1}\n`;
-
-file += `SECRET=${secret}`;
-
-await Bun.write(".env.local", file);
-console.log("✅");

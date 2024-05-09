e690b5cfd71ecce33afb954e59796d9d8c60aa6f
Julian Martinez <julian_martinez28@outlook.com>
2024-05-08 22:15:02 -0700

    update increment example

--- examples/deposits-and-balances/contracts/soroban-increment-contract/src/lib.rs
+++ examples/deposits-and-balances/contracts/soroban-increment-contract/src/lib.rs
@@ -1,5 +1,23 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, log, symbol_short, Env, Symbol};
+use soroban_sdk::{
+    auth, contract, contractimpl, contracttype, log, symbol_short, vec, Address, Env, Symbol, Vec,
+};
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    Contributor(Address),
+    Contributors,
+    Status,
+}
+
+// Define the enum to represent different states
+#[derive(Clone, Copy, PartialEq, Eq)]
+#[contracttype]
+pub enum Status {
+    Active = 0,
+    Inactive = 1,
+}
 
 const COUNTER: Symbol = symbol_short!("COUNTER");
 
@@ -29,6 +47,98 @@ impl IncrementContract {
         // Return the count to the caller.
         count
     }
-}
 
+    pub fn contribute(env: Env, contributor: Address, amount: u64) {
+        contributor.require_auth();
+        // Check if the campaign is active.
+        if Self::get_campaign_status(env.clone()) != Status::Active {
+            panic!("contract is not active");
+        }
+
+        if !Self::is_contributor(env.clone(), contributor.clone()) {
+            Self::push_contributor(env.clone(), contributor.clone());
+        }
+
+        // Record the contribution.
+        env.storage()
+            .instance()
+            .set(&DataKey::Contributor(contributor), &amount);
+    }
+
+    pub fn start_campaign(env: Env) {
+        // Check if the contract is already active.
+        if Self::get_campaign_status(env.clone()) == Status::Active {
+            panic!("contract is already active");
+        }
+
+        // Set the contract status to active.
+        Self::set_status(env.clone(), Status::Active);
+    }
+
+    pub fn stop_campaign(env: Env) {
+        // Check if the contract is already inactive.
+        if Self::get_campaign_status(env.clone()) == Status::Inactive {
+            panic!("contract is already inactive");
+        }
+
+        // Set the contract status to inactive.
+        Self::set_status(env.clone(), Status::Inactive);
+    }
+
+    pub fn get_count(env: Env) -> u32 {
+        env.storage().instance().get(&COUNTER).unwrap_or(0)
+    }
+
+    pub fn set_status(env: Env, status: Status) {
+        env.storage().instance().set(&DataKey::Status, &status);
+    }
+
+    pub fn get_campaign_status(env: Env) -> Status {
+        env.storage()
+            .instance()
+            .get(&DataKey::Status)
+            .unwrap_or(Status::Inactive)
+    }
+
+    pub fn is_contributor(env: Env, contributor: Address) -> bool {
+        env.storage()
+            .instance()
+            .get(&DataKey::Contributor(contributor))
+            .unwrap_or(0)
+            > 0
+    }
+
+    pub fn clear_contributor(env: Env, contributor: Address) {
+        contributor.require_auth();
+        env.storage()
+            .instance()
+            .remove(&DataKey::Contributor(contributor));
+    }
+
+    pub fn get_contribution(env: Env, contributor: Address) -> u64 {
+        env.storage()
+            .instance()
+            .get(&DataKey::Contributor(contributor))
+            .unwrap_or(0)
+    }
+
+    pub fn get_contributors(env: Env) -> Vec<Address> {
+        env.storage()
+            .instance()
+            .get(&DataKey::Contributors)
+            .unwrap_or(vec![&env, env.current_contract_address()])
+    }
+
+    fn push_contributor(env: Env, contributor: Address) {
+        let mut contributors = env
+            .storage()
+            .instance()
+            .get(&DataKey::Contributors)
+            .unwrap_or(vec![&env, contributor.clone()]);
+        contributors.push_back(contributor);
+        env.storage()
+            .instance()
+            .set(&DataKey::Contributors, &contributors);
+    }
+}
 mod test;
--- examples/deposits-and-balances/contracts/soroban-increment-contract/src/test.rs
+++ examples/deposits-and-balances/contracts/soroban-increment-contract/src/test.rs
@@ -1,7 +1,10 @@
 #![cfg(test)]
 
 use super::{IncrementContract, IncrementContractClient};
-use soroban_sdk::{testutils::Logs, Env};
+use soroban_sdk::{
+    testutils::{Address, Logs},
+    Env,
+};
 
 extern crate std;
 
@@ -16,4 +19,15 @@ fn test() {
     assert_eq!(client.increment(), 3);
 
     std::println!("{}", env.logs().all().join("\n"));
+}
+#[test]
+fn test_contribute() {
+    let env = Env::default();
+    env.mock_all_auths();
+    let contract_id = env.register_contract(None, IncrementContract);
+    let client = IncrementContractClient::new(&env, &contract_id);
+    let contributor = <soroban_sdk::Address as Address>::generate(&env);
+    client.start_campaign();
+    client.contribute(&contributor, &100);
+    assert_eq!(client.get_contribution(&contributor), 100);
 }

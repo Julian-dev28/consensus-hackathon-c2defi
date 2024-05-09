672d07fade3ff5fa09ddae749e2faf515a37fa87
Julian Martinez <julian_martinez28@outlook.com>
2024-05-08 22:19:27 -0700

    start staging dapps

---
+++ examples/public-goods/shared/contracts.ts
@@ -0,0 +1,15 @@
+import * as crowdfund from "crowdfund";
+import { SorobanRpc } from "@stellar/stellar-sdk";
+export const client = new crowdfund.Client({
+  ...crowdfund.networks.testnet,
+  rpcUrl: "https://soroban-testnet.stellar.org",
+  publicKey: "GCSXUXZSA2VEXN5VGOWE5ODAJLC575JCMWRJ4FFRDWSTRCJYQK4ML6V3",
+  allowHttp: true,
+});
+
+export const server = new SorobanRpc.Server(
+  "https://soroban-testnet.stellar.org",
+  {
+    allowHttp: "https://soroban-testnet.stellar.org".startsWith("http:"),
+  }
+);
